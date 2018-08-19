b4eb0591d02b480a5d1e3aeafba94a580aac7000
devjohnsonGA <dseth.johnson@gmail.com>
2018-08-19 18:03:14 -0400

    dynamic react profile pic

--- react-boilerplate/src/components/App.js
+++ react-boilerplate/src/components/App.js
@@ -2,12 +2,35 @@ import React, { Component } from "react"
 import '../styles/App.css'
 
 class App extends Component {
-    render(){
+        constructor(props){
+            super(props)
+            this.state = {
+                height: 100
+            }
+        }
+        render(){
+        var {name, age, bio, pic} = this.props;
+        var {height} = this.state
         return(
-            <div>
+            <div className="profile-box">
                 <h1>Dev React App</h1>
+                <h4>Name: {name}</h4>
+                <h4>Age: {age}</h4>
+                <h4>Bio: {bio}</h4>
+                <img src={pic} height={height}/>
+                <br />
+                <button onClick={this.zoomIn.bind(this)}>+</button>
+                <button onClick={this.zoomOut.bind(this)}>-</button>
             </div>
         )
+    }
+    zoomIn() {
+        console.log(this)
+        this.setState({height: this.state.height + 100})
+    }
+    zoomOut() {
+        console.log(this)
+        this.setState({height: this.state.height - 100})
     }
 }
 
--- react-boilerplate/src/index.js
+++ react-boilerplate/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.js";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App name="Devon Johnson" age={36} bio="I like reactjs" pic="https://media.licdn.com/dms/image/C4D03AQGiRj6yQcArgQ/profile-displayphoto-shrink_200_200/0?e=1540425600&v=beta&t=vZaNkbpKOt9Uu2IZRX511HjlAdFIgMhhGCjz4ywDsS8" />, document.getElementById("root"));
